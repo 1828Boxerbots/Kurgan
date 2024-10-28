fa3b1979755a7ab3c12c946230bd7a93c8c4e987
RyanAlterman <38821584+RyanAlterman@users.noreply.github.com>
2024-10-28 15:30:15 -0700

    Uploaded fresh command based template

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -3,42 +3,15 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include "RobotContainer.h"
-#include <frc2/command/button/Trigger.h>
-#include "commands/Autos.h"
-#include "commands/DriveCmd.h"
-#include "commands/LoadCmd.h"
-#include "commands/ShootCmd.h"
 
-RobotContainer::RobotContainer() 
-{
-  // Configure the button bindings
+#include <frc2/command/Commands.h>
+
+RobotContainer::RobotContainer() {
   ConfigureBindings();
 }
 
-void RobotContainer::Init()
-{
-  m_driveSub.Init();
-  m_loadSub.Init();
-  m_shootSub.Init();
+void RobotContainer::ConfigureBindings() {}
 
-  m_driveSub.SetDefaultCommand(DriveCmd(&m_driveSub, &m_driveControl, 1.0).ToPtr()); //Check.
-}
-
-void RobotContainer::ConfigureBindings() 
-{
-  // Configure your trigger bindings here
-  m_driveControl.X().WhileTrue(LoadCmd(&m_loadSub, &m_driveControl, -1.0).ToPtr());
-  m_driveControl.Y().WhileTrue(LoadCmd(&m_loadSub, &m_driveControl, 1.0).ToPtr());
-  m_driveControl.RightTrigger().WhileTrue(ShootCmd(&m_shootSub, &m_driveControl).ToPtr());
-}
-
-int RobotContainer::DPDTSwitch()
-{
-  //No use for it yet (2024).
-  return 0;
-}
-
-frc2::CommandPtr RobotContainer::GetAutonomousCommand() 
-{
-  return NullAutoCmdGroup().ToPtr();
+frc2::CommandPtr RobotContainer::GetAutonomousCommand() {
+  return frc2::cmd::Print("No autonomous command configured");
 }
--- src/main/include/RobotContainer.h
+++ src/main/include/RobotContainer.h
@@ -5,37 +5,13 @@
 #pragma once
 
 #include <frc2/command/CommandPtr.h>
-#include <frc2/command/button/CommandXboxController.h>
-#include "commands/NullAutoCmdGroup.h"
-#include "subsystems/DriveSub.h"
-#include "subsystems/LoadSub.h"
-#include "subsystems/ShootSub.h"
-#include "Constants.h"
 
-/**
- * This class is where the bulk of the robot should be declared.  Since
- * Command-based is a "declarative" paradigm, very little robot logic should
- * actually be handled in the {@link Robot} periodic methods (other than the
- * scheduler calls).  Instead, the structure of the robot (including subsystems,
- * commands, and trigger mappings) should be declared here.
- */
-class RobotContainer 
-{
+class RobotContainer {
  public:
   RobotContainer();
+
   frc2::CommandPtr GetAutonomousCommand();
 
-  void Init();
-
-  int DPDTSwitch();
-
  private:
-// Controller:
-  frc2::CommandXboxController m_driveControl{XboxControl::kPort};
   void ConfigureBindings();
-
-// Subsystems:
-  DriveSub m_driveSub;
-  LoadSub m_loadSub;
-  ShootSub m_shootSub;
 };
